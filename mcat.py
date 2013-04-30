5d5b4fe70113bdbad56d90060713288b518eb81b
Rick Yazwinski <ricky@thinker.ca>
2013-04-30 09:25:09 -0400

    about to change _processes to dict

--- mcat.py
+++ mcat.py
@@ -2,17 +2,21 @@
 """Image Manager CLI Interface
 
 Usage:
-    mcat.py [-t <update>] [<cmd>...]
+    mcat.py [-t <update>] [-c] [-s color:color:color:...] [<cmd>...]
 
 Options:
-    -t <update>     Timeout on checking for updates. Default is 1s.
+    -t <update>                 Timeout on checking for updates. Default is 1s.
+    -c                          Auto colorize commands
+    -s color:color:color:...    Colorize commands as defined. If there are more commands
+                                than colors the 1st will be used as default.
 """
 import select
 import subprocess
 
 try:
-    from termcolor import colored
+    from termcolor import colored, COLORS
 except:
+    COLORS={'green':1} # ignored anyhow but a dict helps keep things clean
     def colored(text, *args, **kwargs):
         return text
 
@@ -25,21 +29,23 @@ class Mcat():
         PATTERN = re.compile(r'''((?:[^ "']|"[^"]*"|'[^']*')+)''')
         return PATTERN.split(cmd)[1::2]
 
-    def __init__(self, commands):
+    def __init__(self, commands, color_iterator):
         for c in commands:
             p=subprocess.Popen(self._split_command(c), stdout=subprocess.PIPE,stderr=subprocess.PIPE,
                 stdin=None, universal_newlines=True)
 
-            self._processes.append([c, p])
+            self._processes.append([c, p, color_iterator.next() if color_iterator else None])
+
+        print self._processes
 
     def run(self, timeout=1.0):
         while True:
             stderr_list = []
             stdout_list = []
-            for [cmd, p] in self._processes:
+            for [cmd, p, color] in self._processes:
                 if p.poll() is not None:
                     print colored('Cmd [%s] exited.' % cmd, 'red')
-                    self._processes.remove([cmd,p])
+                    self._processes.remove([cmd, p, color])
                 else:
                     stderr_list.append(p.stderr)
                     stdout_list.append(p.stdout)
@@ -52,11 +58,27 @@ class Mcat():
 
             for e in read_list:
                 line=e.readline().strip()
+                print 'line color is ' + color
                 if e in stderr_list:
-                    out=colored(line, attrs=['reverse'])
+                    out=colored(line, color=color, attrs=['reverse'])
                 else:
-                    out=line
+                    out=colored(line, color=color)
                 print out
+
+def color_picker(color_list=None):
+    import itertools
+
+    if not color_list:
+        for color_list in itertools.repeat(COLORS.keys()):
+            for list_elem in color_list:
+                yield list_elem
+    else:
+        for color in color_list:
+            yield color
+
+        while True:
+            yield color_list[0]
+
 
 
 if __name__ == '__main__':
@@ -64,10 +86,22 @@ if __name__ == '__main__':
     from docopt import docopt
 
     arguments = docopt(__doc__)
+    print arguments
 
     try:
         if len(arguments['<cmd>']):
-            mcat = Mcat(arguments['<cmd>'])
+            color_iterator = None
+            auto_colorize = arguments.get('-c')
+            if auto_colorize:
+                color_iterator = color_picker()
+            else:
+                cmd_line_colors = arguments['-s']
+                if len(cmd_line_colors):
+                    # may be multiple -s on cli, so join them with a :, then create a list
+                    color_list = ':'.join(cmd_line_colors).split(':')
+                    color_iterator = color_picker(color_list)
+
+            mcat = Mcat(arguments['<cmd>'], color_iterator)
 
             timeout = arguments.get('-t')
             if timeout is not None:
